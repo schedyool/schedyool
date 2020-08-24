2d2ce05cb24e64606f01dd4a261475d1a84dc789
calebren <calebaren@gmail.com>
2020-08-23 21:31:19 -0700

    Redeploy

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -20,7 +20,6 @@ const Home = (): any => {
             <Link href="/scheduler">
                 <Controls.Button text="Schedyool!" color="default" />
             </Link>
-            
         </Page>
     );
 };
