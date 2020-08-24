342cf7ef74570acebcba3bf71cdcedc57bb3ae4e
calebren <calebaren@gmail.com>
2020-08-23 21:23:11 -0700

    Home.tsx update

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -8,13 +8,13 @@ const Home = (): any => {
     return (
         <Page title="🎓 Welcome to Schedyool!" subtitle="A school scheduling app for COVID-19." >
             <Typography paragraph>
-              Thanks to the requirements of blended virtual and physical learning, principals have the
-              daunting task of scheduling students while satisfying several constraints:
+                Thanks to the requirements of blended virtual and physical learning, principals have the
+                daunting task of scheduling students while satisfying several constraints:
           </Typography>
-          <Typography paragraph>
-            Please see the instructions below to get started.
+            <Typography paragraph>
+                Please see the instructions below to get started.
           </Typography>
-          <Link href="/instructions">
+            <Link href="/instructions">
                 <Controls.Button text="Get Started" color="default" />
             </Link>
         </Page>
