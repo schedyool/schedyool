dcb6efc88b6fbf7713c3a626aa74dcc4fa5933d3
calebren <calebaren@gmail.com>
2020-08-25 12:40:38 -0700

    Update Home.tsx

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -22,13 +22,12 @@ const Home = (): any => {
             </Link>
 
             <Typography paragraph variant="body2">
-            DISCLAIMER:  By using Schedyool, you acknowledge that Schedyool comes with no express or 
-implied warranty.  There is no warranty of any kind concerning the fitness of the Schedyool results
-for any use.  There is no guaranty that Schedyool will function as intended.  In particular,
-in some cases Schedyool may produce no schedule at all.  You acknowledge that schedules produced
-by Schedyool may not satisfy all constraints submitted, and 
-that any schedule produced by Schedyool will be manually reviewed by the school's
-principal for appropriateness before being implemented.
+                DISCLAIMER:  By using Schedyool, you acknowledge that Schedyool comes with no express or
+                implied warranty.  There is no warranty of any kind concerning the fitness of the Schedyool results
+                for any use.  There is no guaranty that Schedyool will function as intended.  In particular,
+                in some cases Schedyool may produce no schedule at all.  You acknowledge that schedules produced
+                that any schedule produced by Schedyool will be manually reviewed by the school's
+                principal for appropriateness before being implemented.
 
           </Typography>
         </Page>
